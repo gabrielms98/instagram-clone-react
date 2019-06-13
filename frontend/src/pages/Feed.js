2c9ce6a505f8e81d95d18ffd4644ecc29088be6a
Gabriel M Silva <gmbiel1@gmail.com>
2019-06-13 13:12:30 -0300

    [feature] Real time update when upload or like

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 import more from '../assets/more.svg';
@@ -7,62 +9,73 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component{
+
+    state ={
+        feed: [],
+    };
+
+    async componentDidMount(){
+        this.registerToSocket();
+
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data });
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        //post, like
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likePost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post._id === likePost._id ? likePost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => { 
+        api.post(`/posts/${id}/like`)
+    }
+
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Gabriel Martins</span>
-                            <span className="place">Viçosa</span>
-                        </div>
+                {this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                    <img src={more} alt="Mais"/>
-                    </header>
+                        <img src={more} alt="Mais"/>
+                        </header>
 
-                    <img src="http://localhost:3333/files/wallpaper2.jpg" alt=""/>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt=""/>
+                                </button>
+                                <img src={comment} alt=""/>
+                                <img src={send} alt=""/>
+                            </div>
 
-                        <strong>900 curtidas</strong>
-                        <p>
-                            Um post aleatorio ai.
-                            <span>#instagram-clone-react</span>
-                        </p>
-                    </footer>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtag}</span>
+                            </p>
+                        </footer>
                 </article>
-
-                                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Gabriel Martins</span>
-                            <span className="place">Viçosa</span>
-                        </div>
-
-                    <img src={more} alt="Mais"/>
-                    </header>
-
-                    <img src="http://localhost:3333/files/wallpaper2.jpg" alt=""/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                    
-                        </div>
-                        <strong> 900 curtidas</strong>
-                        <p>
-                             Um post aleatorio ai.
-                             <span>#instagram-clone-react</span>
-                        </p>
-                    </footer>
-                </article>
+                ))}
             </section>
         );
     }
